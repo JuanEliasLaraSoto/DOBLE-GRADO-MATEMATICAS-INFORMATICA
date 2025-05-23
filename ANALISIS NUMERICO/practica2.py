5bfa75aa55e2198add4b839a364d3dcf96a33032
juanelara <juaneliaslarasoto@uma.es>
2025-05-23 13:43:41 +0200

    Exercises 3 and 4

--- ANALISIS NUMERICO/practica2.py
+++ ANALISIS NUMERICO/practica2.py
@@ -548,3 +548,156 @@ leyenda=['N = 320']
 leyenda.append('exacta')
 legend(leyenda)
    
+
+
+#####EJERCICIO 3###################################
+def ABM3(a, b, fun, N, y0):
+    y = zeros(N+1)
+    t = zeros(N+1)
+    f = zeros(N+1)
+    h = (b - a)/float(N) 
+    t[0] = a
+    y[0] = y0
+    f[0] = fun(a, y[0])
+    for k in range(2): # como el método AM3 es de orden 4, hay que hallar y_1 e y_2 con un método unipaso de orden >= 3
+        t[k+1] = t[k] + h
+        k1 = fun(t[k], y[k])
+        k2 = fun(t[k] + h/2, y[k] + h/2*k1)
+        k3 = fun(t[k] + h/2, y[k] + h/2*k2)
+        k4 = fun(t[k+1], y[k] + h*k3)
+        y[k+1] = y[k] + h/6*(k1 + 2*k2 + 2*k3 + k4)
+        f[k+1] = fun(t[k+1], y[k+1]) # MUY IMPORTANTE: siempre se me olvida esta línea
+    for k in range(2, N):
+        Ck = y[k] + h/24*(19*f[k] - 5*f[k-1] + f[k-2])
+        t[k+1] = t[k] + h
+        pred = y[k] + h/12*(23*f[k] - 16*f[k-1] + 5*f[k-2]) # predicción (método AB3)
+        y[k+1] = h*(9/24)*fun(t[k+1], pred) + Ck # correción (método AM3)
+        f[k+1] = fun(t[k+1], y[k+1])
+    return (t, y)
+
+y0 = 0.0
+a = 0.0
+b = 5.0
+malla=[10,20,40,80,160]
+
+figure()
+
+for N in malla:
+    tini = perf_counter()
+
+    (t, y) = ABM3(a, b, fun, N, y0)
+    
+    
+    tfin = perf_counter()
+    plot(t, y, "-*")
+    
+    h=(b-a)/float(N)
+    
+    ye = exacta(t)
+
+    
+    # Calculo del error cometido
+    error = max(abs(y-ye))
+    tcpu=tfin-tini 
+    
+    print('N = '+str(N))
+    print('Error='+str(error))
+    print('Tiempo CPU='+str(tcpu))
+
+    
+    
+    if N > malla[0]:
+        order=(log(errorold)-log(error))/log(2)
+        print('orden aprox ' +str(order))
+    print('---------------------')
+    errorold=error
+    
+
+plot(t,ye)
+leyenda=['N = '+str(N) for N in malla]
+leyenda.append('exacta')
+legend(leyenda)
+   
+######## EJERCICIO 4
+def AM3_sis(a, b, fun, N, y0):
+    Y = zeros((len(Y0), N+1))
+    t = zeros(N+1)
+    F = zeros((len(Y0), N+1))
+    h = (b - a)/float(N) 
+    t[0] = a
+    Y[:,0] = y0
+    F[:,0] = fun(a, Y[:,0])
+    maxiter = 0
+    for k in range(2):
+        t[k+1] = t[k] + h
+        k1 = fun(t[k], Y[:,k])
+        k2 = fun(t[k] + h/2, Y[:,k] + h/2*k1)
+        k3 = fun(t[k] + h/2, Y[:,k] + h/2*k2)
+        k4 = fun(t[k+1], Y[:,k] + h*k3)
+        Y[:,k+1] = Y[:,k] + h/6*(k1 + 2*k2 + 2*k3 + k4)
+        F[:,k+1] = fun(t[k+1], Y[:,k+1])
+    for k in range(2, N):
+        t[k+1] = t[k] + h
+        Ck = Y[:,k] + h/24*(19*F[:,k] - 5*F[:,k-1] + F[:,k-2])
+        eps = 1.e-12
+        error = eps + 1
+        iter = 0
+        z = Y[:,k]
+        while(error >= eps and iter < 200):
+            znew = 9/24*h*fun(t[k+1], z) + Ck
+            error = max(abs(z - znew)) # OJO: LA NORMA EN \R^2 ES DISTINTA (tomamos la norma infinito)
+            z = znew
+            iter += 1
+        if iter == 200:
+            print('El método de punto fijo no ha convergido')
+        maxiter = max(maxiter, iter)
+        Y[:,k+1] = z
+        F[:,k+1] = fun(t[k+1], Y[:,k+1])
+    return (t, Y, maxiter)
+
+def F(t, Y):
+    F1 = 0.25*Y[0] - 0.01*Y[0]*Y[1]
+    F2 = -Y[1] + 0.01*Y[0]*Y[1]
+    return array([F1, F2])
+
+a = 0.
+b = 20.
+malla = [20, 40, 80, 160, 320, 640]
+Y0 = array([80, 30])
+k = len(malla)
+
+
+# método AM3
+
+print('\n\n\nEJERCICIO 4 (AM3)')
+
+figure('EJERCICIO 4 (AM3)')
+
+for i in range(k):
+    N = malla[i]
+    tini = perf_counter()
+    (t, Y, maxiter) = AM3_sis(a, b, F, N, Y0)
+    tfin = perf_counter()
+    
+    subplot(121)
+    plot(t,Y[0],t,Y[1])
+    xlabel('t')
+    ylabel('x,y')
+    legend(['Presas','Depredadores'])
+    
+    subplot(122)
+    plot(Y[0],Y[1])
+    xlabel('presas')
+    ylabel('depredadores')
+    legend(['trayectoria'])
+    
+    print('\nN = ' + str(N))
+    print('Paso de malla: ' + str((b-a)/N))
+    print('Tiempo CPU: ' + str(tfin-tini))
+   
+
+legend(['N = ' + str(N) for N in malla])
+subplot(121)
+legend(leyenda)
+subplot(122)
+legend(leyenda)
