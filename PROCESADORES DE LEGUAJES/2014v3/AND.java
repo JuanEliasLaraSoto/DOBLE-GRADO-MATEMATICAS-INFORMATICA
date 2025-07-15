0f688d2c867cf7ee9dfbcf10de478b740a7ea959
juanelara <juaneliaslarasoto@uma.es>
2025-07-15 08:57:54 +0200

    2014 EXAM

---
+++ PROCESADORES DE LEGUAJES/2014v3/AND.java
@@ -0,0 +1,28 @@
+public class AND extends Condicion {/////PIENSA EN LOQ HACE LUEGO EL IF BASICAMENTE
+    public  AND(AST izq,AST der){
+            super(izq, der,"AND");
+
+    }
+    ///recuerda q evaluamos en cortocircuito
+    public void generarCTD(){
+        if(izq!=null){
+            izq.generarCTD();
+        }
+        ////caso false cond1---CORTOCIRCUITO,DEJO DE EVALUAR Y ME VOY A LA PARTE DEL ELSE
+      
+        /////caso true cond1
+        Generador.etiq(((Condicion)izq).getVF().getV());
+        if(der!=null){
+            der.generarCTD();
+        }
+        Generador.etiq(((Condicion)izq).getVF().getF());
+        Generador.salto(((Condicion)der).getVF().getF());
+
+
+        ///PARTE DONDE ENTRARA IF ES.GETV  Y DONDE ENTRA EL ELSE ES GETF
+       this.vf=new DosEtiq(((Condicion)der).getVF().getV(), ((Condicion)der).getVF().getF());///////LAS DOS Q NO SE HAN USADO
+
+
+    }
+    
+}
